686681f49425d764eac7801f32415233c400ef5a
piyush rawat <rawatpiyush2023@gmail.com>
2026-07-17 15:55:18 +0530

    models and login and register routes setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,16 +5,19 @@ import {Server} from "socket.io";
 import cors from "cors";
 
 import connectToSocket from "./controllers/socketManager.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+app.set("port",(process.env.PORT || 8000));
+
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({ limit:"40kb", extended: true }));
 
-app.set("port",(process.env.PORT || 8000));
+app.use("/api/users",userRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,60 @@
+import {User} from '../models/userModel.js';
+import bcrypt, {hash} from 'bcrypt';
+import httpStatus from "http-status";
+import crypto from 'crypto';
+
+
+const loginUser = async(req,res) =>{
+
+    const {username,password} = req.body;
+    if(!username || !password){
+        return res.status(httpStatus.BAD_REQUEST).json({message:"Please provide username and password"});
+    }
+
+    try{
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(httpStatus.NOT_FOUND).json({message:"User not found"});
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(httpStatus.UNAUTHORIZED).json({message:"Invalid credentials"});
+        }
+
+        const token = crypto.randomBytes(20).toString('hex');
+        user.token = token;
+        await user.save();
+        return res.status(httpStatus.OK).json({message:"Login successful", token:token});
+    
+    }catch(e){
+        return res.status(500).json({message:`Something went wrong ${e}`});
+    }
+}
+
+
+const registerUser = async (req,res) =>{
+    const {name,username,password} = req.body;
+
+    try{
+        const existingUser = await User.findOne({username: req.body.username});
+        if(existingUser){
+            return res.status(httpStatus.FOUND).json({message: "User already exists"});
+        }
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+
+        const newUser = new User({
+            name:req.body.name,
+            username:req.body.username,
+            password:hashPassword,
+        });
+
+        await newUser.save();
+        return res.status(httpStatus.CREATED).json({message:"User registered successfully"});
+
+    }catch(e){
+        return res.status(500).json({message:`Something went wrong ${e}`});
+    }
+}
+
+export {loginUser,registerUser};

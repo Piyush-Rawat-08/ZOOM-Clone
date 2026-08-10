29605b73c7fe278152230c89376f235964897da3
piyush rawat <rawatpiyush2023@gmail.com>
2026-08-11 01:53:12 +0530

    meetingModel updated & meetingController build

---
+++ backend/src/controllers/meetingController.js
@@ -0,0 +1,61 @@
+import Meeting from '..models/meetingModel.js';
+
+const add_to_activity = async (req, res) => {
+    try {
+        const {
+            user_id,
+            meeting_id,
+            title,
+            date,
+            status,
+            isScheduled,
+            scheduledDate
+        } = req.body;
+        const meetingStatus = isScheduled ? 'scheduled' : 'ongoing';
+        const newMeeting = new Meeting({
+            user_id: user_id,
+            meeting_id: meeting_id,
+            title: title || "Instant Meeting",
+            date: date,
+            status: meetingStatus,
+            scheduled_for: scheduledDate || null,
+        })
+        await newMeeting.save();
+        return res.status(httpStatus.CREATED).json({ message: "Meeting added to activity", meeting: newMeeting });
+    }
+    catch (err) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "Something went wrong", error: err });
+    }
+};
+
+const update_activity_end = async (req, res) => {
+    try {
+        const { meeting_id } = req.body;
+
+        const updatedMeeting = await Meeting.findOneAndUpdate(
+            { meeting_id: meeting_id },
+            { status: 'completed' },
+            { ended_at: Date.now() },
+            { new: true }
+        );
+        return res.status(200).json({ message: "meeting completed", meeting: updatedMeeting });
+    }
+    catch (err) {
+        return res.status(500).json({ message: "Error updating meeting", error: err });
+    }
+};
+
+const get_all_activity = async (req, res) => {
+    try {
+        const { user_id } = req.query;
+
+        const history = await Meeting.find({ user_id: user_id }).sort({ date: -1 });
+
+        return res.status(200).json({ message: "History fetched successfully", history: history });
+    }
+    catch (e) {
+        return res.status(500).json({ message: "Error fetching history", error: e });
+    }
+};
+
+export { add_to_activity, update_activity_end, get_all_activity };
--- backend/src/models/meetingModel.js
+++ backend/src/models/meetingModel.js
@@ -1,19 +1,34 @@
-import mongoose,{Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const meetingSchema = new Schema({
-    user_id:{
-        type:String
+    user_id: {
+        type: String
     },
-    meeting_id:{
-        type:String,
-        required:true,
+    meeting_id: {
+        type: String,
+        required: true,
     },
-    date:{
-        type:Date,
-        default:Date.now,
-        required:true,
+    title: {
+        type: String,
+        default: "Instant Meeting",
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['scheduled', 'ongoing', 'completed'],
+        default: 'ongoing',
+    },
+    scheduled_for: {
+        type: Date,
+    },
+    ended_at: {
+        type: Date,
     }
-})
+});
 
-const Meeting = mongoose.model('Meeting',meetingSchema);
-export {Meeting};
+const Meeting = mongoose.model('Meeting', meetingSchema);
+export { Meeting };
